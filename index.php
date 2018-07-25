9260f4ed530c500dd75dd31cb6f80c6683d2fd80
Marcel S. Henselin <marcel@henselin.de>
2018-07-25 14:10:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,16 +11,28 @@ if (!file_exists('vendor/autoload.php')) {
 	$f3 = \F3::instance();
 	
 	$f3->config('config/config.ini');
-	/* */
-	foreach(glob("config-enabled/*.ini") as $filename){
+
+	foreach(glob("config/conf-enabled/*.ini") as $filename){
 		$f3->config($filename);
 	}
-	/* */
 	
 	$f3->config('config/routes.ini');
-	foreach(glob("routes-enabled/*.ini") as $filename){
+	foreach(glob("config/routes-enabled/*.ini") as $filename){
 		$f3->config($filename);
 	}
+	
+	Multilang::instance();
+	
+	$f3->set('ONREROUTE',function($url,$permanent) use($f3,$ml){
+		$f3->clear('ONREROUTE');
+		//TODO log the old routing !!!
+		Multilang::instance()->reroute($url,$permanent);
+	});
+	
+	//current language -> Multilang::instance()->current;
+	//primary language -> Multilang::instance()->primary;
+	//autodetected language -> Multilang::instance()->auto;
 	
 	$f3->run();
 }
+
